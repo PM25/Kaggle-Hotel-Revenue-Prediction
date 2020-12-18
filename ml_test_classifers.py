ed67d0ad98a486c46a441c971bab9ec8a5c53abd
PY <pyhuang97@gmail.com>
2020-12-18 23:38:27 +0800

    modify datapreprocessing part.

--- ml_test_classifers.py
+++ ml_test_classifers.py
@@ -1,12 +1,12 @@
 #%%
 from utils import *
-from datapreprocessing import processing_data_reservation_status
+from datapreprocessing import processing_data
 
 from sklearn.model_selection import train_test_split
 
 # start from here!
 if __name__ == "__main__":
-    (X_np, y_np), _ = processing_data_reservation_status()
+    (X_np, y_np), _ = processing_data(binary=True)
     train_X, test_X, train_y, test_y = train_test_split(
         X_np, y_np, test_size=0.25, random_state=0
     )
